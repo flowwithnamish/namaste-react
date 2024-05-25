4d4a5ba77685e58a071b097ba581c13592f449b6
flowwithnamish <namish.develop@gmail.com>
2024-05-25 16:04:41 +0530

    Class1

---
+++ App.js
@@ -0,0 +1,30 @@
+/**
+ * <div id="parent">
+ * <div id="child">
+ * <h1>I am h1 tag</h1>
+ * <h2>I am h1 tag</h2>
+ * </div>
+ * <div id="child2">
+ * <h1>I am h1 tag</h1>
+ * <h2>I am h1 tag</h2>
+ * </div>
+ * </div>
+ * 
+ */
+
+const parent = React.createElement("div",{id:'parent'},[
+React.createElement("div",{id:'child'},[
+React.createElement("h1",{},"i am h1 tag"),
+React.createElement("h2",{},"i am h2 tag")
+]),React.createElement("div",{id:'child2'},[
+    React.createElement("h1",{},"i am h1 tag"),
+    React.createElement("h2",{},"i am h2 tag")
+    ])]);
+
+const heading = React.createElement(
+    "h1",
+    {id:'heading'
+
+},"hello world react!");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
