5ccf39ceed2d5e2a9c3673bd273191ed743a3b9c
flowwithnamish <namish.develop@gmail.com>
2024-05-26 19:14:42 +0530

    episode-02

--- App.js
+++ App.js
@@ -1,20 +1,9 @@
-/**
- * <div id="parent">
- * <div id="child">
- * <h1>I am h1 tag</h1>
- * <h2>I am h1 tag</h2>
- * </div>
- * <div id="child2">
- * <h1>I am h1 tag</h1>
- * <h2>I am h1 tag</h2>
- * </div>
- * </div>
- * 
- */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div",{id:'parent'},[
 React.createElement("div",{id:'child'},[
-React.createElement("h1",{},"i am h1 tag"),
+React.createElement("h1",{},"This is flow with Namish"),
 React.createElement("h2",{},"i am h2 tag")
 ]),React.createElement("div",{id:'child2'},[
     React.createElement("h1",{},"i am h1 tag"),
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # flowwithnamish
+
+# Parcel
+- Dev build
+- Local server
+- HMR = Hot module replacement
+- File watching algo - written in C++
+- Caching - faster builds
+- Image optimization
+- Minification 
+- Bundling
+- Compress 
+- Consistent Hashing
+- Code splitting
+- Differential bundling - supports older browser
+- Diagonostic
+- Error Handling
+- HTTPs
+- Tree shaking - remove unused code
+- Different dev and prod build
